6cf39cceb64e118310a46c3da027ed210a1a72a8
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-08-20 13:28:04 +0700

    button

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './color.scss'
 import './index.scss'
+import './component.scss'
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client'
 import {
   BrowserRouter,
--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -6,11 +6,12 @@ export default function Login() {
     <div className='full-screen center-all'>
       <div className='flex flex-col center-all w-fit h-fit'>
         <img className='w-72 mb-4' alt='LinkHEdIn' src={mainLogo} />
-        <div className='flex flex-col w-fit px-2 py-4 space-x-2'>
+        <div className='flex flex-col w-96 px-2 py-4 space-x-2 center-all'>
           <input className='text-input' type={"email"} placeholder={"email"} />
           <div className='form-space-y'></div>
           <input className='text-input' type={"password"} placeholder={"passwword"} />
-          <div className='button'></div>
+          <div className='form-space-y'></div>
+          <div className='btn'>Sign In</div>
         </div>
       </div>
     </div>
