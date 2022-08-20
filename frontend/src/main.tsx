4cb6b6ebea5733bf7826110af2837abdf68ff66b
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-08-20 16:18:35 +0700

    login page done

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './color.scss'
-import './index.scss'
-import './component.scss'
+import './styles/color.scss'
+import './styles/index.scss'
+import './styles/component.scss'
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client'
 import {
   BrowserRouter,
@@ -11,19 +11,26 @@ import {
   Route,
 } from "react-router-dom"
 import Login from './pages/login/Login'
+import { useLocalStorage } from './hooks/useLocalStorage'
 
 const client = new ApolloClient({
   uri: 'http://localhost:8080/query',
   cache: new InMemoryCache(),
 })
 
+const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <ApolloProvider client={client}>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/' element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </ApolloProvider>
+  <div data-theme={theme}>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/' element={<App />} />
+        </Routes>
+      </BrowserRouter>
+      <App />
+    </ApolloProvider>
+  </div>
 )
--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -3,16 +3,24 @@ import mainLogo from '../../assets/mainLogo.png'
 
 export default function Login() {
   return (
-    <div className='full-screen center-all'>
-      <div className='flex flex-col center-all w-fit h-fit'>
+    <div className='full-screen center-all bg-base-100'>
+      <div className='flex flex-col center-all w-fit h-fit px-4'>
         <img className='w-72 mb-4' alt='LinkHEdIn' src={mainLogo} />
-        <div className='flex flex-col w-96 px-2 py-4 space-x-2 center-all'>
+        <div className='flex flex-col w-96 py-4 center-all'>
           <input className='text-input' type={"email"} placeholder={"email"} />
           <div className='form-space-y'></div>
           <input className='text-input' type={"password"} placeholder={"passwword"} />
           <div className='form-space-y'></div>
-          <div className='btn'>Sign In</div>
+          <div className='place-self-start link'>Forgot passowrd?</div>
+          <div className='form-space-y'></div>
+          <div className='btn btn-primary'>Sign In</div>
+          <div className='form-space-y'></div>
+          <div className='text-base-content'>OR</div>
+          <div className='form-space-y'></div>
+          <div className='btn btn-neutral'>Sign in with google</div>
         </div>
+        <div className='mt-8'></div>
+        <p className='text-base-content'>New to linkedin? <span className='link'>Join now!</span></p>
       </div>
     </div>
   )
