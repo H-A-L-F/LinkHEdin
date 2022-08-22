3140ecfce9cd08ee7b63b954e5f96878f1e646c8
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-08-22 11:19:05 +0700

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,15 +6,16 @@ import {
 import { useLocalStorage } from "./hooks/useLocalStorage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
   const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
   
-  
-
   return (
     <BrowserRouter>
+      <ToastContainer />
       <div className="full-screen bg-base-100" data-theme={theme}>
         <Routes>
           <Route path='login' element={<Login />} />
