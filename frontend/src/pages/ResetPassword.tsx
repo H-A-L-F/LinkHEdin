3e007e2824245b84f790a0cda0bbea50cbaeb1ef
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-08-24 02:34:33 +0700

    add code to reset pass page

--- frontend/src/pages/ResetPassword.tsx
+++ frontend/src/pages/ResetPassword.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function ResetPassword() {
+    const [authorized, setAuthorized] = useState(false)
+
     function handleResetPass(e: any) {
 
     }
@@ -9,6 +11,7 @@ export default function ResetPassword() {
 
     }
 
+    if(!authorized) return <ResetPassCode /> 
     return (
         <form action="" onSubmit={handleResetPass}>
             <div className='flex flex-col w-96 py-4 center-all'>
@@ -17,6 +20,23 @@ export default function ResetPassword() {
                 <input className='text-input' type={"password"} placeholder={"Repeat Password"} name={"confpass"} />
                 <div className='form-space-y'></div>
                 <button className='btn btn-primary'>Reset password</button>
+            </div>
+        </form>
+    )
+}
+
+
+function ResetPassCode() {
+    function handleSubmitCode() {
+
+    }
+
+    return (
+        <form action="" onSubmit={handleSubmitCode}>
+            <div className='flex flex-col w-96 py-4 center-all'>
+                <input className='text-input' type={"text"} placeholder={"Code"} name={"code"} />
+                <div className='form-space-y'></div>
+                <button className='btn btn-primary'>Submit</button>
             </div>
         </form>
     )
