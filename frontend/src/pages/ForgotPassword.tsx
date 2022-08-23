03117adffada2f662fd9036ebe2c84958c4310f8
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-08-24 01:55:14 +0700

    add back to login funcitonality

--- frontend/src/pages/ForgotPassword.tsx
+++ frontend/src/pages/ForgotPassword.tsx
@@ -1,5 +1,7 @@
 import { useMutation } from '@apollo/client'
 import React from 'react'
+import { useNavigate } from 'react-router'
+import { ROUTE } from '../config/constants'
 import { toastError } from '../config/toast'
 import { useBackEnd } from '../hooks/useBackEnd'
 import { REQ_CHANGE_PASS_QUERY } from '../query/user'
@@ -7,6 +9,7 @@ import { REQ_CHANGE_PASS_QUERY } from '../query/user'
 export default function ForgotPassword() {
     const [reqChangePassFunc] = useMutation(REQ_CHANGE_PASS_QUERY)
     const { reqChangePass } = useBackEnd()
+    const navigate = useNavigate()
 
     function handleForgotPass(e: any) {
         e.preventDefault()
@@ -27,6 +30,10 @@ export default function ForgotPassword() {
         return true
     }
 
+    function handleBack() {
+        navigate(ROUTE.ROUTE_LOGIN)
+    }
+
     return (
         <form action="" onSubmit={handleForgotPass}>
             <div className='flex flex-col w-96 py-4 center-all'>
@@ -34,7 +41,7 @@ export default function ForgotPassword() {
                 <div className='form-space-y'></div>
                 <button className='btn btn-primary'>Reset password</button>
                 <div className='form-space-y'></div>
-                <div className='btn btn-neutral'>Back</div>
+                <div className='btn btn-neutral' onClick={handleBack}>Back</div>
             </div>
         </form>
     )
