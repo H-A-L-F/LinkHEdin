fd18f25197bb033891481b8feed4a61e11bbf000
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-08-26 15:02:52 +0700

    education ui

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -5,16 +5,54 @@ import EditProfileBg from '../components/Profile/EditProfileBg';
 import { toastError } from '../config/toast';
 import { useAuth } from '../hooks/useAuth';
 import { useBackEnd } from '../hooks/useBackEnd';
+import { HiPencil, HiTrash } from "react-icons/hi";
+import IconButton from '../components/Buttons/IconButton';
 
 export default function Profile() {
     const { id } = useParams()
     const { user } = useAuth()
 
     return (
-        <div className='box'>
-            <div className='profile'>
-                <EditProfileBg />
-                <AvatarProfile />
+        <div>
+            <div className='box'>
+                <div className='profile'>
+                    <EditProfileBg />
+                    <AvatarProfile />
+                </div>
+            </div>
+            <div className='h-4'></div>
+            <div className='box'>
+                <div className='header'>
+                    Education
+                    <IconButton Icon={HiPencil} />
+                </div>
+                <div className='content'>
+                    <div className='education'>
+                        <div className='info'>
+                            <div className='font-semibold text-lg'>Binus</div>
+                            <div className='text-md'>CompSci</div>
+                            <div className='text-sm'>02 Jan - 30 Jan</div>
+                        </div>
+                        <div className='flex flex-row'>
+                            <IconButton Icon={HiTrash} />
+                            <div className='w-2'></div>
+                            <IconButton Icon={HiPencil} />
+                        </div>
+                    </div>
+                    <div className='divider'></div>
+                    <div className='education'>
+                        <div className='info'>
+                            <div className='font-semibold text-lg'>Binus</div>
+                            <div className='text-md'>CompSci</div>
+                            <div className='text-sm'>02 Jan - 30 Jan</div>
+                        </div>
+                        <div className='flex flex-row'>
+                            <IconButton Icon={HiTrash} />
+                            <div className='w-2'></div>
+                            <IconButton Icon={HiPencil} />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
