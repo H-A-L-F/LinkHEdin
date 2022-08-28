be4dc28c467229f57426d19ad00f5769896ddf65
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-08-28 18:10:51 +0700

    experience backend

--- backend/go-graphql-jwt/graph/model/models_gen.go
+++ backend/go-graphql-jwt/graph/model/models_gen.go
@@ -2,6 +2,20 @@
 
 package model
 
+type Experience struct {
+	ID             string `json:"ID"`
+	UserID         string `json:"UserID"`
+	Title          string `json:"Title"`
+	EmploymentType string `json:"EmploymentType"`
+	CompanyName    string `json:"CompanyName"`
+	Location       string `json:"Location"`
+	Active         bool   `json:"Active"`
+	StartYear      string `json:"StartYear"`
+	EndYear        string `json:"EndYear"`
+	Industry       string `json:"Industry"`
+	Description    string `json:"Description"`
+}
+
 type NewLink struct {
 	Link   string `json:"link"`
 	Code   int    `json:"code"`
@@ -42,4 +56,17 @@ type NewEducation struct {
 	Grade        float64 `json:"Grade"`
 	Activities   string  `json:"Activities"`
 	Description  string  `json:"Description"`
+}
+
+type NewExperience struct {
+	UserID         string `json:"UserID"`
+	Title          string `json:"Title"`
+	EmploymentType string `json:"EmploymentType"`
+	CompanyName    string `json:"CompanyName"`
+	Location       string `json:"Location"`
+	Active         bool   `json:"Active"`
+	StartYear      string `json:"StartYear"`
+	EndYear        string `json:"EndYear"`
+	Industry       string `json:"Industry"`
+	Description    string `json:"Description"`
 }
