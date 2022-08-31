6c6720c34a13007cf3d7906391427e73070dcdc4
Arya Jayadi <arya.jayadi2013@gmail.com>
2022-09-01 04:20:06 +0700

    start a post ui

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,21 +1,33 @@
 import React from 'react'
-import { HiPhotograph } from "react-icons/hi";
+import { HiPhotograph, HiPlay } from "react-icons/hi";
 
 export default function Home() {
   return (
     <div className='box'>
       <div className='flex flex-col'>
-        <div className='flex flex-row'>
-          <div className='outline'>
+        <div className='btn-plain add-outline'>
+          <div className='bg'></div>
+          <div className='place-self-start py-4'>
             Start a post
           </div>
         </div>
+        <div className='h-4'></div>
         <div className='flex flex-row'>
           <div className='btn-plain'>
             <div className='bg'></div>
-            <div className='content'>
-              Photo
-              <HiPhotograph />
+            <div className='flex flex-row py-4'>
+              <HiPhotograph className='fill-info' />
+              <div className='w-2'></div>
+              <div className='font-semibold'>Photo</div>
+            </div>
+          </div>
+
+          <div className='btn-plain'>
+            <div className='bg'></div>
+            <div className='flex flex-row py-4'>
+              <HiPlay className='fill-success' />
+              <div className='w-2'></div>
+              <div className='font-semibold'>Photo</div>
             </div>
           </div>
         </div>
