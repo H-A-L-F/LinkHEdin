e9a8e7aa3a7ef688906ba333ced6bbb63b13319b
Arya Jayadi <arya.jayadi2013@gmail.com>
2022-09-01 02:45:49 +0700

    start a post (test commit)

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { HiPhotograph } from "react-icons/hi";
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='box'>
+      <div className='flex flex-col'>
+        <div className='flex flex-row'>
+          <div className='outline'>
+            Start a post
+          </div>
+        </div>
+        <div className='flex flex-row'>
+          <div className='btn-plain'>
+            <div className='bg'></div>
+            <div className='content'>
+              Photo
+              <HiPhotograph />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
