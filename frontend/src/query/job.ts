5d3f665cce237f6e72891ab422a1c6a0a336a778
Arya Jayadi <arya.jayadi2013@gmail.com>
2022-09-01 17:08:24 +0700

    job and notification query

---
+++ frontend/src/query/job.ts
@@ -0,0 +1,32 @@
+import { gql } from "@apollo/client";
+
+export const FIND_JOB_QUERY = gql`
+  query FindJob {
+    jobs {
+      id
+      title
+      companyName
+      location
+      createdAt
+      photoProfile
+    }
+  }
+`;
+
+export const CREATE_JOB_MUTATION = gql`
+  mutation CreateJob(
+    $title: String!
+    $companyName: String!
+    $location: String!
+    $photoProfile: String!
+  ) {
+    createJob(
+      input: {
+        title: $title
+        companyName: $companyName
+        location: $location
+        photoProfile: $photoProfile
+      }
+    )
+  }
+`;
