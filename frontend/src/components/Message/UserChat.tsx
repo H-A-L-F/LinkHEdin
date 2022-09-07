6efa79be5e108f2d7f7a14b132686ccd9d86eb02
Arya Jayadi <arya.jayadi2013@gmail.com>
2022-09-07 08:16:01 +0700

    .

---
+++ frontend/src/components/Message/UserChat.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useAuth } from '../../hooks/useAuth'
+
+export default function UserChat() {
+    const {user} = useAuth()
+
+    return (
+        <div className='outline-holder'>
+            <div className='chat-item' tabIndex={1}>
+                <div className='inv-avatar'>
+                    <img src={user.PhotoProfile} className='inv-avatar-image' />
+                </div>
+                <div className='w-2'></div>
+                <div className='flex flex-col'>
+                    <div className='text-md font-semibold'>{user.name}</div>
+                    <div className='text-sm font-medium'>{user.email}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- frontend/src/pages/Message.tsx
+++ frontend/src/pages/Message.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { HiPencilAlt, HiOutlineDotsHorizontal } from "react-icons/hi";
+import UserChat from '../components/Message/UserChat';
 import { useAuth } from '../hooks/useAuth';
 
 export default function Message() {
     const { user } = useAuth()
+    
     return (
         <div className='box w-full'>
             <div className='flex flex-col h-full justify-start'>
@@ -27,18 +29,7 @@ export default function Message() {
                 <div className='divider'></div>
                 <div className='flex flex-row center-all w-full h-full justify-start'>
                     <div className='flex flex-col w-96 h-full'>
-                        <div className='outline-holder'>
-                            <div className='chat-item' tabIndex={1}>
-                                <div className='inv-avatar'>
-                                    <img src={user.PhotoProfile} className='inv-avatar-image' />
-                                </div>
-                                <div className='w-2'></div>
-                                <div className='flex flex-col'>
-                                    <div className='text-md font-semibold'>{user.name}</div>
-                                    <div className='text-sm font-medium'>{user.email}</div>
-                                </div>
-                            </div>
-                        </div>
+                        <UserChat />
                     </div>
                     <div className='divider-h'></div>
                     <div className='flex flex-row justify-between w-full'>
