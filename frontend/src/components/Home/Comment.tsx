e81fb4c215e045054f697878d82661c8e4cdbf3a
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-09-09 15:06:01 +0700

    comment box

---
+++ frontend/src/components/Home/Comment.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Comment() {
+    return (
+        <div className='box py-4'>
+            <div className='comment-box'>
+                <textarea className='input' placeholder='Comment here...'/>
+            </div>
+        </div>
+    )
+}
--- frontend/src/components/Home/Post.tsx
+++ frontend/src/components/Home/Post.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { HiPlus, HiThumbUp, HiChatAlt, HiShare, HiPaperAirplane } from "react-icons/hi";
 import { useAuth } from '../../hooks/useAuth';
+import Comment from './Comment';
 import { PostInterface } from './PostInterface';
 
 interface PostCompInterface {
@@ -8,95 +9,100 @@ interface PostCompInterface {
 }
 
 export default function Post({ ps }: PostCompInterface) {
+    const [openComment, setOpenComment] = useState(false)
 
     return (
-        <div className='box'>
-            <div className='flex flex-col'>
-                <div className='flex flex-row justify-between center-all'>
-                    <div className='flex flex-row center-all'>
-                        <div className='sq-avatar'>
-                            <img src={ps.User.PhotoProfile} className='sq-avatar-image' />
-                        </div>
-                        <div className='w-2'></div>
-                        <div className='flex flex-col'>
-                            <div className='text-md font-semibold'>{ps.User.name}</div>
-                            <div className='text-sm font-normal'>{ps.User.email}</div>
-                            <div className='text-sm font-normal'>{ps.User.FollowedUser.length}</div>
-                        </div>
-                    </div>
-                    <div className='btn-plain w-fit h-fit py-2'>
-                        <div className='bg'></div>
+        <React.Fragment>
+            <div className='box'>
+                <div className='flex flex-col'>
+                    <div className='flex flex-row justify-between center-all'>
                         <div className='flex flex-row center-all'>
-                            <HiPlus size={20} />
+                            <div className='sq-avatar'>
+                                <img src={ps.User.PhotoProfile} className='sq-avatar-image' />
+                            </div>
                             <div className='w-2'></div>
-                            <div className='font-semibold'>
-                                Follow
+                            <div className='flex flex-col'>
+                                <div className='text-md font-semibold'>{ps.User.name}</div>
+                                <div className='text-sm font-normal'>{ps.User.email}</div>
+                                <div className='text-sm font-normal'>{ps.User.FollowedUser.length}</div>
+                            </div>
+                        </div>
+                        <div className='btn-plain w-fit h-fit py-2'>
+                            <div className='bg'></div>
+                            <div className='flex flex-row center-all'>
+                                <HiPlus size={20} />
+                                <div className='w-2'></div>
+                                <div className='font-semibold'>
+                                    Follow
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className='divider'></div>
-                <div className='text-sm font-normal'>{ps.text}</div>
-                <div className='h-2'></div>
-                {ps.AttachmentType === "image" &&
-                    <img src={ps.AttachmentLink} className='w-full h-96 rounded-md' />
-                }
-                {ps.AttachmentType === "video" &&
-                    <video src={ps.AttachmentLink} className='w-full h-96 rounded-md' controls />
-                }
-                <div className='flex flex-row justify-between'>
-                    <div></div>
-                    <div className='flex flex-row'>
-                        <div className='text-sm font-normal'>{ps.comments} comments</div>
-                        <div className='w-2'></div>
-                        <div className='text-sm font-normal'>{ps.sends} shares</div>
-                    </div>
-                </div>
-                <div className='divider'></div>
-                <div className='flex flex-row justify-around'>
-                    <div className='btn-plain w-fit h-fit py-2'>
-                        <div className='bg'></div>
-                        <div className='flex flex-row center-all'>
-                            <HiThumbUp size={20} />
+                    <div className='divider'></div>
+                    <div className='text-sm font-normal'>{ps.text}</div>
+                    <div className='h-2'></div>
+                    {ps.AttachmentType === "image" &&
+                        <img src={ps.AttachmentLink} className='w-full h-96 rounded-md' />
+                    }
+                    {ps.AttachmentType === "video" &&
+                        <video src={ps.AttachmentLink} className='w-full h-96 rounded-md' controls />
+                    }
+                    <div className='flex flex-row justify-between'>
+                        <div></div>
+                        <div className='flex flex-row'>
+                            <div className='text-sm font-normal'>{ps.comments} comments</div>
                             <div className='w-2'></div>
-                            <div className='font-semibold'>
-                                Like
-                            </div>
+                            <div className='text-sm font-normal'>{ps.sends} shares</div>
                         </div>
                     </div>
-                    <div className='btn-plain w-fit h-fit py-2'>
-                        <div className='bg'></div>
-                        <div className='flex flex-row center-all'>
-                            <HiChatAlt size={20} />
-                            <div className='w-2'></div>
-                            <div className='font-semibold'>
-                                Comment
+                    <div className='divider'></div>
+                    <div className='flex flex-row justify-around'>
+                        <div className='btn-plain w-fit h-fit py-2'>
+                            <div className='bg'></div>
+                            <div className='flex flex-row center-all'>
+                                <HiThumbUp size={20} />
+                                <div className='w-2'></div>
+                                <div className='font-semibold'>
+                                    Like
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className='btn-plain w-fit h-fit py-2'>
-                        <div className='bg'></div>
-                        <div className='flex flex-row center-all'>
-                            <HiShare size={20} />
-                            <div className='w-2'></div>
-                            <div className='font-semibold'>
-                                Share
+                        <div className='btn-plain w-fit h-fit py-2' onClick={() => { setOpenComment((prev) => !prev) }}>
+                            <div className='bg'></div>
+                            <div className='flex flex-row center-all'>
+                                <HiChatAlt size={20} />
+                                <div className='w-2'></div>
+                                <div className='font-semibold'>
+                                    Comment
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className='btn-plain w-fit h-fit py-2'>
-                        <div className='bg'></div>
-                        <div className='flex flex-row center-all'>
-                            <HiPaperAirplane size={20} />
-                            <div className='w-2'></div>
-                            <div className='font-semibold'>
-                                Send
+                        <div className='btn-plain w-fit h-fit py-2'>
+                            <div className='bg'></div>
+                            <div className='flex flex-row center-all'>
+                                <HiShare size={20} />
+                                <div className='w-2'></div>
+                                <div className='font-semibold'>
+                                    Share
+                                </div>
+                            </div>
+                        </div>
+                        <div className='btn-plain w-fit h-fit py-2'>
+                            <div className='bg'></div>
+                            <div className='flex flex-row center-all'>
+                                <HiPaperAirplane size={20} />
+                                <div className='w-2'></div>
+                                <div className='font-semibold'>
+                                    Send
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <div className='h-2'></div>
+            <Comment />
+        </React.Fragment>
     )
 }
 
