da956d30b603d25fa18cd2f923bdfaaf9c4348eb
Arya Jayadi <arya.jayadi2013@gmail.com>
2022-09-16 20:41:18 +0700

    search query

---
+++ frontend/src/query/search.ts
@@ -0,0 +1,51 @@
+import { gql } from "@apollo/client";
+
+export const SEARCH_USER_QUERY = gql`
+  query SearchUser($query: String!) {
+    searchUser(query: $query) {
+      id
+      name
+    }
+  }
+`;
+
+export const SEARCH_QUERY = gql`
+  query Search($query: String!) {
+    search(query: $query) {
+      user {
+        id
+        name
+        email
+        PhotoProfile
+        FollowedUser
+        ConnectedUser
+        RequestConnect
+        Headline
+        ProfileViews
+        BgPhotoProfile
+      }
+      post {
+        id
+        text
+        AttachmentLink
+        User {
+          id
+          name
+          email
+          PhotoProfile
+          FollowedUser
+          ConnectedUser
+          RequestConnect
+          Headline
+          ProfileViews
+          BgPhotoProfile
+        }
+        hashtag
+        likes
+        sends
+        comments
+        createdAt
+      }
+    }
+  }
+`;
